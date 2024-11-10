30a232c7bde5b149fd7c3cf441a3d61a5fbfc9da
Franvilla03 <123724558+Franvilla03@users.noreply.github.com>
2024-11-10 20:49:17 +0100

    Update main.js

--- UD3/07_events/main.js
+++ UD3/07_events/main.js
@@ -1,3 +1,27 @@
 window.onload = function(e) {
-    console.log('documento cargado')
+    console.log('documento cargado');
+    
+    document.getElementById('enlace_1').onclick = function() {
+        toggleSection('contenidos_1', 'enlace_1');
+    };
+
+    document.getElementById('enlace_2').onclick = function() {
+        toggleSection('contenidos_2', 'enlace_2');
+    };
+
+    document.getElementById('enlace_3').onclick = function() {
+        toggleSection('contenidos_3', 'enlace_3');
+    };
+}
+function toggleSection(sectionId, linkId) {
+    const section = document.getElementById(sectionId);
+    const link = document.getElementById(linkId);
+
+    if (section.style.display === 'none') {
+        section.style.display = 'block';
+        link.innerHTML = 'Ocultar contenidos';
+    } else {
+        section.style.display = 'none';
+        link.innerHTML = 'Mostrar contenidos';
+    }
 }
