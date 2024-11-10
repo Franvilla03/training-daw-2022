6e9a6bc465e3b9754cc1526227f94d31ace28fa6
Franvilla03 <123724558+Franvilla03@users.noreply.github.com>
2024-11-10 20:37:51 +0100

    Update main.js

--- UD1/13_multiplications/main.js
+++ UD1/13_multiplications/main.js
@@ -1,8 +1,16 @@
 function printMultiplicationTables() {
-
+    let result = '';
+    
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            result += (i * j).toString().padStart(4, ' '); 
+        }
+    result += '\n';
+    }
+    console.log(result);
 }
 
-printMultiplicationTables()
+printMultiplicationTables();
 // expected output
 //     1   2   3   ...
 // 1   1   2   3   ...
