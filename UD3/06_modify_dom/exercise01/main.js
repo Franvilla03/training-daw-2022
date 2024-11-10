d13f918109031f69152b88347473f0567fcc461f
Franvilla03 <123724558+Franvilla03@users.noreply.github.com>
2024-11-10 20:46:57 +0100

    Update main.js

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,21 @@
-window.onload = function(e) {
-    console.log('documento cargado')
+document.querySelectorAll("article h2")[1].textContent = "Nuevo Título del Segundo H2";
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
-}
+const usernameElement = document.getElementById("username");
+console.log(usernameElement);
+
+const firstParagraphsInArticles = document.querySelectorAll("article .first");
+firstParagraphsInArticles.forEach((element) => {
+    element.style.color = "blue";
+});
+
+const liItems = document.querySelectorAll("li.item");
+console.log(liItems);
+
+const buttonsInButtonsClass = document.querySelectorAll(".buttons button");
+console.log(buttonsInButtonsClass);
+const firstParagraph = document.querySelector("p.first");
+firstParagraph.style.backgroundColor = "lightgray";
+
+buttonsInButtonsClass.forEach((button) => {
+    button.style.color = "white";
+});
